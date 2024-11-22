d69caecd1f09320f35f34ee03d9951c7b9ac3cc3
v4p0 <160601667+v4p0@users.noreply.github.com>
2024-11-22 09:09:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript',
+    'O Javascript é uma linuagem deprogramação.'
+    )
+
+    criaCartao(
+        'Programação',
+        'O que é uma função?',
+        'Uma função é um bloco de código que executa uma tarefa.'
+    )
+
+    criaCartao(
+        'Geograia',
+        'Qual é a capital da França',
+        'A capital da França é Paris.'
+    )
+
+    criaCartao(
+         'Lingua Inglesa',
+         'Como  se diz oi em inglês?',
+         'Oi em inglês se diz HI (RAI)'
+    )
